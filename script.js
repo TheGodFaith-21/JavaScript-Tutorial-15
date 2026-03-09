57ccaec885fc1be7705ed43309194919fac7c4d8
TGF Onins <fngsabulao0921@gmail.com>
2026-03-09 12:22:27 +0800

    code script on mouse out hover

--- script.js
+++ script.js
@@ -6,26 +6,33 @@ console.log('');
 
 console.log("ONLOAD EVENT LISTENER");
 console.log('');
-function funcName(){
+/*function funcName(){
     alert("Page Loaded");
     let title = document.querySelector(".title");
     title.innerHTML="Hello World";
 }
+    */
 console.log('');
 console.log("ONCLICK EVENT LISTENER");
 console.log('');
-
+/*
 function submit(){
     let title = document.querySelector(".title");
     title.innerHTML="JavaScript Tutorial 15";  
     alert("H1 Change");
 }
+    */
 console.log('');
-console.log("ONMOUSEOVER EVENT LISTENER");
+console.log("ONMOUSEOVER and ONMOUSEOUT EVENT LISTENER");
 console.log('');
 
 function mHov(){
     let title = document.querySelector(".title");
     title.innerHTML="You Hover the text";
-    alert("Hover This");
+    //alert("Hover This");
+}
+function umHov(){
+    let title = document.querySelector(".title");
+    title.innerHTML="You Hover This";
+    //alert("Hover This");
 }
