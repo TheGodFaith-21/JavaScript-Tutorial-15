0ae9f9ee8156a2409516a2088a2690a0e99dd8d5
TGF Onins <fngsabulao0921@gmail.com>
2026-03-09 21:51:34 +0800

    added code / modify the code in tutorial challenge 15

--- scriptAct.js
+++ scriptAct.js
@@ -1,5 +1,9 @@
 alert("JS Tutorial Challenge 15");
 
+let body = document.querySelector("body");
+body.style.textAlign = "Center";
+
+
 function personalInformation(){
     let decision = document.querySelector("#chkAgree").checked;
     let firstName = document.querySelector("#txtFname").value;
@@ -7,11 +11,16 @@ function personalInformation(){
     let date =  document.querySelector("#txtBday").value;
     let sex = document.querySelector("input[name=sex]:checked").value;
     let slcCountry = document.querySelector("#slcCountry").value;
-        
+
+            
     if(decision){
+        document.querySelector("#fName").innerHTML = "Name: " + firstName +' '+lastName;
         console.log(firstName +' '+lastName);
+        document.querySelector("#bDay").innerHTML = "Birth date: " + date;
         console.log(date);
+        document.querySelector("#sex").innerHTML = "Gender: " + sex;
         console.log(sex);
+        document.querySelector("#country").innerHTML = "Country: " + slcCountry;
         console.log(slcCountry);
     }else{
         alert("Please Check the Privacy Policy");
