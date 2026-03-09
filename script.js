d1ea977e98da3cea577ce993d8c7a9bbf6ec3129
TGF Onins <fngsabulao0921@gmail.com>
2026-03-09 13:02:13 +0800

    code script on input event listener

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ console.log('');
 
 console.log("ONLOAD EVENT LISTENER");
 console.log('');
-
+/*
 function funcName(){
     alert("Page Loaded");
     let title = document.querySelector(".title");
     title.innerHTML="Hello World";
 }
-
+*/
 console.log('');
 console.log("ONCLICK EVENT LISTENER");
 console.log('');
-
+/*
 function submit(){
     let title = document.querySelector(".title");
     title.innerHTML="JavaScript Tutorial 15";  
     alert("H1 Change");
 }
-
+*/
 console.log('');
 console.log("ONMOUSEOVER and ONMOUSEOUT EVENT LISTENER");
 console.log('');
-
+/*
 function mHov(){
     let title = document.querySelector(".title");
     title.innerHTML="You Hover the text";
@@ -37,23 +37,33 @@ function umHov(){
     title.innerHTML="You Hover This";
     //alert("Hover This");
 }
-
+*/
 console.log('');
 console.log("ONFOCUS EVENT LISTENER");
 console.log('');
-
+/*
 function fInput(){
     let inp = document.querySelector("input"); 
     inp.style.background = "blue";
     inp.style.color = "white";
 }
-
+*/
 console.log('');
 console.log("ONBLUR EVENT LISTENER");
 console.log('');
-
+/*
 function bInput(){
     let inp = document.querySelector("input"); 
     inp.style.background = "green";
     inp.style.color = "yellow";
+}
+*/
+console.log('');
+console.log("ONINPUT EVENT LISTENER");
+console.log('');
+
+function oInput(){
+    let inp = document.querySelector("input"); 
+    let p = document.querySelector("p");
+    p.innerHTML = inp.value;
 }
