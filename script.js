737de0bd4bd98e77468694b94ae46f2976558817
TGF Onins <fngsabulao0921@gmail.com>
2026-03-09 11:20:54 +0800

    coding script for onload event listener

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 console.log("JavaScript Tutorial 15");
 
 console.log('');
-console.log("DOM Event Listeners");
+console.log("DOM EVENT LISTENER");
+console.log('');
+
+console.log("ONLOAD EVENT LISTENER");
+console.log('');
+function funcName(){
+    alert("Page Loaded");
+    let title = document.querySelector(".title");
+    title.innerHTML="Hello World";
+}
+console.log('');
+console.log("ONCLICK EVENT LISTENER");
 console.log('');
