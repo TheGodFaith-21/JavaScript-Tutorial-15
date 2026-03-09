f29d97bab627e1eeb2f0c1b89af61b5b2e008169
TGF Onins <fngsabulao0921@gmail.com>
2026-03-09 12:37:25 +0800

    code script input onblur event listener

--- script.js
+++ script.js
@@ -6,22 +6,23 @@ console.log('');
 
 console.log("ONLOAD EVENT LISTENER");
 console.log('');
-/*function funcName(){
+
+function funcName(){
     alert("Page Loaded");
     let title = document.querySelector(".title");
     title.innerHTML="Hello World";
 }
-    */
+
 console.log('');
 console.log("ONCLICK EVENT LISTENER");
 console.log('');
-/*
+
 function submit(){
     let title = document.querySelector(".title");
     title.innerHTML="JavaScript Tutorial 15";  
     alert("H1 Change");
 }
-    */
+
 console.log('');
 console.log("ONMOUSEOVER and ONMOUSEOUT EVENT LISTENER");
 console.log('');
@@ -35,4 +36,24 @@ function umHov(){
     let title = document.querySelector(".title");
     title.innerHTML="You Hover This";
     //alert("Hover This");
+}
+
+console.log('');
+console.log("ONFOCUS EVENT LISTENER");
+console.log('');
+
+function fInput(){
+    let inp = document.querySelector("input"); 
+    inp.style.background = "blue";
+    inp.style.color = "white";
+}
+
+console.log('');
+console.log("ONBLUR EVENT LISTENER");
+console.log('');
+
+function bInput(){
+    let inp = document.querySelector("input"); 
+    inp.style.background = "green";
+    inp.style.color = "yellow";
 }
